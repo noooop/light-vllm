36fceb852958483537d1962b4a295e0f30fa42f2
noooop <noooop@126.com>
2024-10-10 15:16:30 +0800

    workflow_cls_from_engine_args

--- light_vllm/wde/retriever/modelzoo/gte_qwen/workflow.py
+++ light_vllm/wde/retriever/modelzoo/gte_qwen/workflow.py
@@ -1,6 +1,21 @@
-from light_vllm.wde.decode_only.workflow import DecodeOnlyWorkflow
+from typing import Dict
+
+from light_vllm.wde.decode_only.workflow import DecodeOnlyWorkflow, Workflow
 
 
-class Qwen2Workflow(DecodeOnlyWorkflow):
-    EngineArgs: str = ("light_vllm.wde.retriever.modelzoo."
-                       "gte_qwen.arg_utils:Qwen2EngineArgs")
+class Qwen2Workflow(Workflow):
+
+    @classmethod
+    def workflow_cls_from_engine_args(cls, engine_args: Dict):
+        # gte-Qwen2 and Qwen2 use the same architecture name，Qwen2ForCausalLM.
+        # gte-Qwen2 family may have multiple different architectures.
+        # gte-Qwen2-1.5B-instruct, does not use enable bidirectional.
+        #     I'm not sure if this is a bug
+        # gte-Qwen2-7B-instruct use enable bidirectional
+        if "gte-Qwen2-1.5B-instruct" in engine_args["model"]:
+            engine_args["output_last_hidden_states"] = True
+        elif "gte-Qwen2-7B-instruct" in engine_args["model"]:
+            engine_args["output_last_hidden_states"] = True
+            engine_args["enable_bidirectional"] = True
+
+        return DecodeOnlyWorkflow.workflow_cls_from_engine_args(engine_args)
