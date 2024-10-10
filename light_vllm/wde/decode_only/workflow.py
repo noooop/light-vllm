0cf28dfa71bb6e368ae0f1ef8c26d7a4bfe6bf6b
noooop <noooop@126.com>
2024-10-10 15:18:16 +0800

    workflow_cls_from_engine_args

--- light_vllm/wde/decode_only/workflow.py
+++ light_vllm/wde/decode_only/workflow.py
@@ -1,25 +1,35 @@
+from typing import Dict
+
+from light_vllm.wde.chat.workflow import ChatWorkflow
 from light_vllm.wde.core.workflow import Workflow
 from light_vllm.wde.prefill_only.workflow import PrefillOnlyWorkflow
 
 
-class DecodeOnlyWorkflow(Workflow):
-    EngineArgs: str = "light_vllm.wde.decode_only.arg_utils:DecodeOnlyEngineArgs"
+class DecodeOnlyOutputLastHiddenStatesWorkflow(Workflow):
+    EngineArgs: str = ("light_vllm.wde.decode_only.arg_utils:"
+                       "DecodeOnlyEngineArgs")
     attn_type: str = "DECODER"
 
     @classmethod
     def from_engine(cls, engine):
-        if engine.engine_config.model_config.output_last_hidden_states:
-            workflow = PrefillOnlyWorkflow.from_engine(engine)
+        workflow = PrefillOnlyWorkflow.from_engine(engine)
 
-            if engine.engine_config.model_config.enable_bidirectional:
-                workflow.attn_type = "ENCODER"
-            else:
-                workflow.attn_type = "DECODER"
-
-            workflow.OutputProcessor = (
-                "light_vllm.wde.decode_only.processor."
-                "output_processor:"
-                "DecodeOnlyHiddenStatesOutputProcessor")
-            return workflow
+        if engine.engine_config.model_config.enable_bidirectional:
+            workflow.attn_type = "ENCODER"
         else:
-            raise ValueError("Not supported")
+            workflow.attn_type = "DECODER"
+
+        workflow.OutputProcessor = ("light_vllm.wde.decode_only.processor."
+                                    "output_processor:"
+                                    "DecodeOnlyHiddenStatesOutputProcessor")
+        return workflow
+
+
+class DecodeOnlyWorkflow(Workflow):
+
+    @classmethod
+    def workflow_cls_from_engine_args(cls, engine_args: Dict):
+        if engine_args.get("output_last_hidden_states", False):
+            return DecodeOnlyOutputLastHiddenStatesWorkflow
+        else:
+            return ChatWorkflow
